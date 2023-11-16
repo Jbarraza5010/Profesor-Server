59f4789db6698bb7a1f69c95d105bd73ff75ec21
Jbarraza5010 <jbarraza5010@gmail.com>
2023-11-16 17:11:52 -0600

    botones y glade incluido

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <opencv2/opencv.hpp>
+#include <gtk/gtk.h>
 
 using namespace std;
 using namespace cv;
 
-int main() {
+GtkWidget *window;
+GtkWidget *button1;
+GtkWidget *button2;
+
+void exit_app(){
+    printf("Exit app \n");
+    gtk_main_quit();
+}
+
+void button_clicked1(){
+    printf("algoritmo1 \n");
+}
+
+void button_clicked2(){
+    printf("algoritmo2 \n");
+}
+
+int main(int argc, char *argv[]) {
     // Crea el socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
@@ -78,10 +96,25 @@ int main() {
     // Send the image data to the client
     send(clientSocket, serverImageData.data(), serverImageSize, 0);
 
+    GtkBuilder *builder;
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "/home/tomeito/CLionProjects/Server/algoritmos.glade",NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "myWindow"));
+    button1 = GTK_WIDGET(gtk_builder_get_object(builder, "algoritmo1"));
+    button2 = GTK_WIDGET(gtk_builder_get_object(builder, "algoritmo2"));
+
+    gtk_builder_connect_signals(builder, NULL);
+    g_object_unref(builder);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
     // Cierra los sockets
     close(clientSocket);
     close(serverSocket);
 
     return 0;
 }
-
