18e4c5eddc7af42e6f55663215c211909e3f4693
josuect0212 <joscalvo@estudiantec.cr>
2023-11-16 23:21:04 -0600

    RSA Algorithm

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 #include <openssl/rand.h>
 #include <openssl/aes.h>
 #include <openssl/evp.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
 
 using namespace std;
 using namespace cv;
@@ -16,6 +19,38 @@ GtkWidget *button1;
 GtkWidget *button2;
 
 int clientSocket;
+
+void encryptRSA(const vector<uint8_t>& input, vector<uint8_t>& output, const vector<uint8_t>& publicKey) {
+    RSA *rsa = NULL;
+    BIO *keyBio = BIO_new_mem_buf(publicKey.data(), publicKey.size());
+
+    if (keyBio == NULL) {
+        cerr << "Error creando el objeto BIO" << endl;
+        return;
+    }
+
+    rsa = PEM_read_bio_RSA_PUBKEY(keyBio, NULL, NULL, NULL);
+    BIO_free(keyBio);
+
+    if (rsa == NULL) {
+        cerr << "Error leyendo la clave pública RSA" << endl;
+        return;
+    }
+
+    int inputSize = input.size();
+    int outputSize = RSA_size(rsa);
+    output.resize(outputSize);
+
+    int result = RSA_public_encrypt(inputSize, input.data(), output.data(), rsa, RSA_PKCS1_PADDING);
+
+    if (result == -1) {
+        cerr << "Error encriptando con RSA" << endl;
+        RSA_free(rsa);
+        return;
+    }
+
+    RSA_free(rsa);
+}
 
 void encryptAES(const vector<uint8_t>& input, vector<uint8_t>& output, const vector<uint8_t>& key) {
     EVP_CIPHER_CTX* ctx;
@@ -46,7 +81,7 @@ void button_clicked1(){
     // Carga la imagen de un archivo
     Mat serverImage = imread("/home/jct/Profesor-Server/tec-logo.jpg", IMREAD_UNCHANGED);
 
-    // Convierte la imagen a un set de bytes
+    // Convierte la imagen a un conjunto de bytes
     vector<uint8_t> serverImageData;
     imencode(".jpg", serverImage, serverImageData);
 
@@ -69,12 +104,70 @@ void button_clicked1(){
     uint32_t keySize = aesKey.size();
     send(clientSocket, &keySize, sizeof(keySize), 0);
 
+    uint8_t algorithmIdentifier = 1;
+    send(clientSocket, &algorithmIdentifier, sizeof(algorithmIdentifier), 0);
+
     send(clientSocket, encryptedImageData.data(), encryptedImageSize, 0);
     send(clientSocket, aesKey.data(), keySize, 0);
 }
 
 void button_clicked2(){
-    printf("algoritmo2 \n");
+    // Carga la imagen de un archivo
+    Mat serverImage = imread("/home/jct/Profesor-Server/tec-logo.jpg", IMREAD_UNCHANGED);
+
+    // Convierte la imagen a un conjunto de bytes
+    vector<uint8_t> serverImageData;
+    imencode(".jpg", serverImage, serverImageData);
+
+    // Genera un par de claves RSA
+    RSA *rsa = RSA_generate_key(2048, RSA_F4, NULL, NULL);
+    if (rsa == NULL) {
+        cerr << "Error generando la clave RSA" << endl;
+        close(clientSocket);
+        return;
+    }
+
+    // Obtiene la clave pública en formato DER
+    BIO *bio = BIO_new(BIO_s_mem());
+    i2d_RSAPublicKey_bio(bio, rsa);
+    vector<uint8_t> rsaPublicKey;
+    char buffer[256];
+    int bytesRead;
+    while ((bytesRead = BIO_read(bio, buffer, sizeof(buffer))) > 0) {
+        rsaPublicKey.insert(rsaPublicKey.end(), buffer, buffer + bytesRead);
+    }
+    BIO_free(bio);
+
+    // Encripta la imagen con RSA
+    vector<uint8_t> encryptedImageData;
+    encryptRSA(serverImageData, encryptedImageData, rsaPublicKey);
+
+    // Envía el identificador, el tamaño de la imagen cifrada, la clave RSA pública y la imagen al cliente
+    uint32_t encryptedImageSize = encryptedImageData.size();
+    uint32_t publicKeySize = rsaPublicKey.size();
+
+    cout << "Tamaño de la clave pública RSA a enviar: " << publicKeySize << endl;
+
+    // Envía el tamaño de la imagen cifrada, la clave RSA pública y la imagen al cliente
+    send(clientSocket, &encryptedImageSize, sizeof(encryptedImageSize), 0);
+    send(clientSocket, &publicKeySize, sizeof(publicKeySize), 0);
+
+    // Envía la clave RSA pública al cliente
+    if (send(clientSocket, rsaPublicKey.data(), publicKeySize, 0) == -1) {
+        cerr << "Error enviando la clave pública RSA al cliente" << endl;
+        close(clientSocket);
+        return;
+    }
+
+    // Envía la imagen cifrada al cliente
+    if (send(clientSocket, encryptedImageData.data(), encryptedImageSize, 0) == -1) {
+        cerr << "Error enviando la imagen cifrada al cliente" << endl;
+        close(clientSocket);
+        return;
+    }
+
+    // Limpieza
+    RSA_free(rsa);
 }
 
 int main(int argc, char *argv[]) {
@@ -97,7 +190,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    // Recive la conexion del cliente
+    // Recibe la conexión del cliente
     if (listen(serverSocket, 10) == -1) {
         cerr << "Error recibiendo conexiones" << endl;
         close(serverSocket);
@@ -106,7 +199,7 @@ int main(int argc, char *argv[]) {
 
     cout << "Esperando conexiones..." << endl;
 
-    // Acepyta la conexion
+    // Acepta la conexión
     sockaddr_in clientAddress;
     socklen_t clientAddressSize = sizeof(clientAddress);
     clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&clientAddress), &clientAddressSize);
@@ -123,7 +216,7 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "/home/jct/Profesor-Server/algoritmos.glade",NULL);
+    gtk_builder_add_from_file(builder, "/home/jct/Profesor-Server/algoritmos.glade", NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "myWindow"));
     button1 = GTK_WIDGET(gtk_builder_get_object(builder, "algoritmo"));
